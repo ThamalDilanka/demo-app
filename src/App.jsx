891788a77f868b79982a62d963de6d27dad50d17
prasad20001205 <prasadsamaranayake184@gmail.com>
2025-09-05 19:55:35 +0530

    feat: Remove duplicate Vite logo and adjust layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,14 @@ function App() {
   return (
     <>
       <div>
+        <p>Vite + React</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
